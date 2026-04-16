9d7a40e0d0666eafc6f1fb00d634992276096e9d
tkusal <thiago.kusal@outlook.com>
2026-04-15 21:48:14 -0300

    Aula 15/04/2026

--- Semana 4/Ex2 - PARA CASA.c
+++ Semana 4/Ex2 - PARA CASA.c
@@ -1,5 +1,5 @@
 /*
-    Escreva um algortimo em C que recebça:
+    Escreva um algortimo em C que receba:
 
     Matrícula do Aluno:
     Idade do Aluno:
@@ -7,5 +7,34 @@
     Endereço do Aluno:
 
     O programa deverá escrever na tela a ficha completa do aluno, incluindo nome, idade, matricula e endereço.
-
 */
+
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int matricula, idade;
+    char nome[31], endereco[31];
+
+    printf("Informe a matricula: ");
+    scanf("%d", &matricula);
+
+    printf("Informe a idade: ");
+    scanf("%d", &idade);
+    getchar();
+
+    printf("Informe o nome: ");
+    fgets(nome, sizeof(nome), stdin);
+    nome[ strcspn(nome, "\n") ] = '\0';
+
+    printf("Informe o endereço: ");
+    fgets(endereco, sizeof(endereco), stdin);
+    endereco[ strcspn(endereco, "\n") ] = '\0';
+
+    printf(" O aluno %s, matricula %d, tem %d anos e mora no endereço %s", nome, matricula, idade, endereco);
+
+    return 0;
+}
--- Semana 4/PARA CASA/HOMELAB-4.c
+++ Semana 4/PARA CASA/HOMELAB-4.c
@@ -6,3 +6,31 @@
 
     > Bubble Sort <
 */
+
+#include <stdio.h>
+
+int main() {
+    int num[5], aux;
+
+    for(int i =0; i<5; i++) {
+        printf("Num %d: ", i+1);
+        scanf("%d", &num[i]);
+    }
+
+    //Bubble Sort
+    for(int i =0; i < 5-1; i++) {
+        for (int j= 0; j < 5-1-i; j++) {
+            if (num[j] > num[j+1]) {
+                aux = num[j];
+                num[j] = num[j+1];
+                num[j+1] = aux;
+            }
+        }
+    }
+
+    for(int i =0; i<5; i++) {
+        printf("%d ", num[i]);
+    }
+
+    return 0;
+}
